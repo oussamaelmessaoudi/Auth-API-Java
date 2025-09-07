384789d1c694e8fd6e66fc251943eaba6989afca
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-09-07 17:54:04 +0100

    Create RefreshTokenRepo for managing refresh token persistence

--- src/main/java/com/jwt/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/jwt/jwt/repository/RefreshTokenRepository.java
@@ -1,4 +1,11 @@
 package com.jwt.jwt.repository;
 
-public interface RefreshTokenRepository {
+import com.jwt.jwt.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUserId(UUID userId);
 }
