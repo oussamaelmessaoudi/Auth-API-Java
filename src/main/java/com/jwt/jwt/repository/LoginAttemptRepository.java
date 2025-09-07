35e87fbaefdd930cf260bc91d95fa74324ad7d70
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-09-07 17:53:16 +0100

    Create LoginAttemptRepo or querrying login attempt data

--- src/main/java/com/jwt/jwt/repository/LoginAttemptRepository.java
+++ src/main/java/com/jwt/jwt/repository/LoginAttemptRepository.java
@@ -1,4 +1,10 @@
 package com.jwt.jwt.repository;
 
-public interface LoginAttemptRepository {
+import com.jwt.jwt.entity.LoginAttempt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LoginAttemptRepository extends JpaRepository<LoginAttempt, UUID> {
+    Optional<LoginAttempt> findByUsername(String username);
 }
